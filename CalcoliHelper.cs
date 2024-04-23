38c4f9f991390f05c378274bcd48d31f75571afb
RaoulBongarzoni <raoul.bongarzoni99@gmail.com>
2024-04-23 17:03:26 +0200

    Helper class for base arithmetic + manual input testing

---
+++ CalcoliHelper.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal static class CalcoliHelper
+    {
+
+        //il tipo generico come mostrato a lezione//
+        public static T Somma<T>(T num1, T num2) where T : INumber<T>    
+        {
+            return num1 + num2;
+
+        }
+        //per completezza di esercizio dividerò per tipi come richiesto
+        //**********************************************************+//
+
+
+        //**************** SOMME *********************//
+        public static int SommaInteri(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+
+        public static double SommaDouble(double num1, double num2)
+        {
+
+            return num1 + num2;
+        }
+
+        //**************** DIFFERENZE *******************//
+
+        public static int DiffInteri(int num1, int num2)
+        {
+
+            return num1 - num2;
+        }
+
+        public static double DiffDouble(double num1, double num2)
+        {
+
+            return num1 - num2;
+        }
+
+        //**************** MOLTIPLICAZIONI *******************//
+
+        public static double MultDouble(double num1, double num2)
+        {
+
+            return num1 * num2;
+        }
+
+        public static int MultInteri(int num1, int num2)
+        {
+
+            return num1 * num2;
+        }
+
+
+        //******************** VALORE ASSOLUTO ************************//
+
+
+        public static int AbsoluteInt(int num1)
+        {
+            int temp = num1;
+            if (temp > 0)
+            {
+                return temp;
+            }
+            else if (temp < 0)
+            {
+                return (-1) * temp;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public static double AbsoluteDouble(double num1)
+        {
+            double temp = num1;
+            if (temp > 0)
+            {
+                return temp;
+            }
+            else if (temp < 0)
+            {
+                return (-1) * temp;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        //******************** MINORE *************************//
+
+
+        public static int MinInt(int num1, int num2)
+        {
+            int temp1 = num1;
+            int temp2 = num2;
+            if (temp1 > temp2)
+            {
+                return temp2;
+            }
+            else if (temp1 < temp2)
+            {
+                return temp1;
+            }
+            else
+            {
+                return temp1;
+            }
+
+        }
+
+        public static double MinDouble(double num1, double num2)
+        {
+            double temp1 = num1;
+            double temp2 = num2;
+            if (temp1 > temp2)
+            {
+                return temp2;
+            }
+            else if (temp1 < temp2)
+            {
+                return temp1;
+            }
+            else
+            {
+                return temp1;
+            }
+
+        }
+
+        //****************** MAGGIORE **********************//
+
+        public static int MaxInt(int num1, int num2)
+        {
+            int temp1 = num1;
+            int temp2 = num2;
+            if (temp1 < temp2)
+            {
+                return temp2;
+            }
+            else if (temp1 > temp2)
+            {
+                return temp1;
+            }
+            else
+            {
+                return temp1;
+            }
+
+        }
+
+        public static double MaxDouble(double num1, double num2)
+        {
+            double temp1 = num1;
+            double temp2 = num2;
+            if (temp1 < temp2)
+            {
+                return temp2;
+            }
+            else if (temp1 > temp2)
+            {
+                return temp1;
+            }
+            else
+            {
+                return temp1;
+            }
+
+        }
+
+        //******************* ESPONENZIALE *********************//
+
+        public static double ExpInt(int base1, int expo) 
+
+        {
+            double baseInterna = base1;
+            double expoInterna = expo;
+
+
+            if (expo == 0)  //ESPONENTE 0 -> RISULTATO 1
+            {
+                return 1;
+            }
+
+            if (base1 == 0) //BASE 0 ->RISULTATO è 0
+            {
+                return 0;
+            }
+
+
+            if (expo < 0) //ESPONENTE NEGATIVO RIPORTA A FRAZIONE
+            {
+                baseInterna = 1 / baseInterna;
+                expoInterna = -expoInterna;
+            }
+
+            double risultato = 1;
+            for (int i = 0; i < expoInterna; i++)  //MOLTIPLICA LA BASE PER SE STESSA EXPO VOLTE
+            {
+                risultato *= baseInterna;
+            }
+
+            return risultato;
+
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace csharp_calcolatrice
+﻿using System.ComponentModel;
+
+namespace csharp_calcolatrice
 {
     internal class Program
     {
@@ -14,8 +16,8 @@
             - Moltiplicazione di due numeri double
             - Valore assoluto di un numero intero
             - Valore assoluto di un numero double
-            - Minimo tra due numeri interi
-            - Minimo tra due numeri double
+            - Maximo tra due numeri interi
+            - Maximo tra due numeri double
             - Massimo tra due numeri interi
             - Massimo tra due numeri double
             Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi di programmazione ad oggetti che abbiamo visto oggi…Quale? Scrivetelo in un commento all’interno del codice! :faccia_nerd:
@@ -24,6 +26,196 @@
             Aggiungete il metodo per l’elevamento a potenza che prende come parametri due numeri interi (base ed esponente) e ritorna la base elevata all’esponente. Occhio che sia la base che l’esponente potrebbero essere uguali a zero oppure numeri negativi!
             (caso esponente = 0 e base = 0 anche se in matematica sarebbe un numero indefinito usiamo l’approccio informatico che lo calcola come 1)
             Buon lavoro!*/
+
+
+            Console.WriteLine("====== BENVENUTO ======");
+            string index = "";
+            while (index != "esci") {
+
+                Console.WriteLine("Scegli quale funzione vuoi testare: \n");
+                Console.WriteLine("1 - Somma di due numeri interi\r\n" +
+                    "2 - Somma di due numeri double\r\n" +
+                    "3 - Differenza tra due numeri interi\r\n" +
+                    "4 - Differenza tra due numeri double\r\n" +
+                    "5 - Moltiplicazione di due numeri interi\r\n" +
+                    "6 - Moltiplicazione di due numeri double\r\n" +
+                    "7 - Valore assoluto di un numero intero\r\n" +
+                    "8 - Valore assoluto di un numero double\r\n" +
+                    "9 - Minimo tra due numeri interi\r\n" +
+                    "10 - Minimo tra due numeri double\r\n" +
+                    "11 - Massimo tra due numeri interi\r\n" +
+                    "12 - Massimo tra due numeri double\r\n" +
+                    "13 - Elevamento a potenza\r\n");
+                Console.WriteLine("Inserisci 'esci' per terminare il programma \n");
+                index = Console.ReadLine();
+                Console.WriteLine("============");
+
+
+                if (index == "1") {
+
+                    Console.WriteLine("1 - Somma di due numeri interi\r\n" );
+                    Console.WriteLine("inserisci i due numeri da sommare");
+                    var num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.SommaInteri(num1, num2)} \n");
+
+                }
+
+                if (index == "2")
+                {
+
+                    Console.WriteLine("2 - Somma di due numeri double\r\n");
+                    Console.WriteLine("inserisci i due numeri da sommare");
+                    var num1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.SommaDouble(num1, num2)} \n");
+
+                }
+
+                if (index == "3")
+                {
+
+                    Console.WriteLine("3 - Differenza tra due numeri interi\r\n");
+                    Console.WriteLine("inserisci i due numeri da sottrarre");
+                    var num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.DiffInteri(num1, num2)}  \n");
+
+                }
+
+                if (index == "4")
+                {
+
+                    Console.WriteLine("4 - Differenza tra due numeri double\r\n");
+                    Console.WriteLine("inserisci i due numeri da sottrarre");
+                    var num1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.DiffDouble(num1, num2)}  \n");
+
+                }
+
+                if (index == "5")
+                {
+
+                    Console.WriteLine("5 - Moltiplicazionetra due numeri interi\r\n");
+                    Console.WriteLine("inserisci i due numeri da moltiplicare");
+                    var num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.MultInteri(num1, num2)}  \n");
+
+                }
+
+                if (index == "6")
+                {
+
+                    Console.WriteLine("6 - Moltiplicazione tra due numeri double\r\n");
+                    Console.WriteLine("inserisci i due numeri da moltiplicare");
+                    var num1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.MultDouble(num1, num2)}   \n");
+
+                }
+
+                if(index == "7")
+                {
+                    Console.WriteLine("7 - Valore assoluto di un numero intero\r\n");
+                    Console.WriteLine("inserisci il numero intero da cui ricavare il valore assoluto");
+                    var num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.AbsoluteInt(num1)}");
+                }
+
+                if (index == "8")
+                {
+                    Console.WriteLine("8 - Valore assoluto di un numero double\r\n");
+                    Console.WriteLine("inserisci il numero intero da cui ricavare il valore assoluto");
+                    var num1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.AbsoluteDouble(num1)}");
+                }
+
+                if (index == "9")
+                {
+
+                    Console.WriteLine("9 - Minimo tra due numeri interi\r\n");
+                    Console.WriteLine("inserisci i due numeri da indicare il Minimo");
+                    var num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.MinInt(num1, num2)}   \n");
+
+                }
+
+                if (index == "10")
+                {
+
+                    Console.WriteLine("10 - Minimo tra due numeri double\r\n");
+                    Console.WriteLine("inserisci i due numeri da indicare il Minimo");
+                    var num1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.MinDouble(num1, num2)}   \n");
+
+                }
+
+                if (index == "11")
+                {
+
+                    Console.WriteLine("11 - Massimo tra due numeri interi\r\n");
+                    Console.WriteLine("inserisci i due numeri da indicare il Massimo");
+                    var num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.MaxInt(num1, num2)}   \n");
+
+                }
+
+                if (index == "12")
+                {
+
+                    Console.WriteLine("12 - Massimo tra due numeri double\r\n");
+                    Console.WriteLine("inserisci i due numeri da indicare il Massimo");
+                    var num1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("ora il secondo");
+                    var num2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("il risultato è:\n");
+                    Console.WriteLine($"{CalcoliHelper.MaxDouble(num1, num2)}   \n");
+
+                }
+
+                if (index == "13")
+                {
+                    Console.WriteLine("12 - elevamento a potenza di un intero\r\n");
+                    Console.WriteLine("== Inserisci la base:\r\n");
+                    var num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("== Inserisci l'esponente:\r\n");
+                    var num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"il numero {num1} elevato a {num2} è : {CalcoliHelper.ExpInt(num1, num2)}   \n");
+
+
+                }
+
+            }
+
+
+
+
+
         }
     }
 }
